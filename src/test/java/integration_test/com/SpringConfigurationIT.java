bc62d73f0102a0cc7ac607ab7269304540cecef7
mukeshkamboj <mukesh.shyamlal@gmail.com>
2015-12-12 21:28:42 +0530

    Added the tesecase in UserControllerIT.java class to get the user based upon the username.

--- src/test/java/integration_test/com/SpringConfigurationIT.java
+++ src/test/java/integration_test/com/SpringConfigurationIT.java
@@ -1,10 +1,15 @@
 package integration_test.com;
 
+import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 import com.Application;
 
@@ -14,4 +19,13 @@ import com.Application;
 @Ignore
 public class SpringConfigurationIT {
 
+	@Autowired
+	protected WebApplicationContext wac;
+
+	protected MockMvc mockMvc;
+
+	@Before
+	public void setup() {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
+	}
 }
